3ff76da9ee2826aec84f52eee87ff852b447d5f4
Michael Feldman <git@maf7.oib.com>
2015-05-03 21:20:25 -0400

    Initial Commit

---
+++ NameThatAirport/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NameThatAirport
+//
+//  Created by Michael Feldman on 5/3/15.
+//  Copyright (c) 2015 Michael Feldman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
